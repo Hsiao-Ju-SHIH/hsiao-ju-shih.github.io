3f1afc7f4f468e14baefac972d8365033a666b58
Hsiao-Ju-SHIH <166608360+Hsiao-Ju-SHIH@users.noreply.github.com>
2024-04-11 19:26:41 +0800

    Add files via upload

---
+++ web-project/scripts/main.js
@@ -0,0 +1,31 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/rowlet-ball.png") {
+        myImage.setAttribute("src", "images/rowlet-another.png");
+    } else {
+        myImage.setAttribute("src", "images/rowlet-ball.png");
+    }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName()
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Rowlet is cute, ${myName}`;
+    }
+}
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Rowlet is cute, ${storedName}`;
+}
+myButton.onclick = () => {
+    setUserName();
+}
